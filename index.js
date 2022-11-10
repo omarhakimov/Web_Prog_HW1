c550c3728ec9e8c6853fd63bfe759af8b324ef49
omarhakimov <omarabduhakim1@gmail.com>
2022-11-11 01:06:34 +0800

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('.img__btn').addEventListener('click', function () {
+	document.querySelector('.cont').classList.toggle('s--signup');
+});
+document.querySelector('.btn-submit').addEventListener('click', () => {
+	alert('Congrats!');
+});
+
+document.querySelector('.btn-signin').addEventListener('click', () => {
+	alert('Congrats!');
+});
